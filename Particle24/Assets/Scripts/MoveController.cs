28b2663c741f75a9a8088c20083086d4710f8722
JackSkylark <jdslaughter44@gmail.com>
2017-09-02 00:22:32 -0500

    init

---
+++ Particle24/Assets/Scripts/MoveController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour {
+
+    public int speed = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Time.deltaTime * speed;
+        this.gameObject.transform.Translate(x, 0, 0);
+	}
+}
